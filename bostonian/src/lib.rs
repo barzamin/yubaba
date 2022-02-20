37679feb562f742bff4df5909d4532620dda4992
erin petra sofiya moon <erin@hecke.rs>
2022-02-20 18:42:34 -0500

    bostonian: messagebox on inject

--- bostonian/src/lib.rs
+++ bostonian/src/lib.rs
@@ -1,10 +1,11 @@
 #![allow(non_snake_case)]
 
-use std::ffi::c_void;
+use std::{ffi::c_void, ptr};
 
 use windows::Win32::{
-    Foundation::{BOOL, HINSTANCE},
+    Foundation::{BOOL, HINSTANCE, HWND, PSTR},
     System::{LibraryLoader::DisableThreadLibraryCalls, SystemServices::DLL_PROCESS_ATTACH},
+    UI::WindowsAndMessaging::{MessageBoxA, MB_OK},
 };
 
 #[no_mangle]
@@ -12,6 +13,15 @@ pub extern "stdcall" fn DllMain(dll_handle: HINSTANCE, reason: u32, _reserved: c
     if reason == DLL_PROCESS_ATTACH {
         unsafe {
             DisableThreadLibraryCalls(dll_handle);
+        }
+
+        unsafe {
+            MessageBoxA(
+                None,
+                PSTR(b"injected!\0".as_ptr()),
+                PSTR(b"woof!\0".as_ptr()),
+                MB_OK,
+            );
         }
     }
 
